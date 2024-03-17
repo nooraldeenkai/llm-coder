e23e5ee821f49e1d7efcf2a497a940ec45ac868c
nooraldeenkai <36330047+nooraldeenkai@users.noreply.github.com>
2024-03-18 01:33:23 +0800

    add feedback service

--- app/services.py
+++ app/services.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from app.models import Feedback
 
 
 client = OpenAI(api_key='')
@@ -18,6 +19,34 @@ def get_answer(prompt):
         },
         {"role": "user",
                  "content": prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+
+    )
+
+    return response.choices[0].message.content
+
+
+def submit_feedback(feedback: Feedback):
+    messages = [
+        {
+            "role": "system",
+                    "content": "you're a smart coding assistant bot designed to generate code that adhere to the user's specifications.\
+                                You should only generate code that adheres to the user's specifications. \
+                                You should not generate code that is not related to the user's specifications. \
+                                You should not generate code that is not related to the user's specifications. \
+                                You should only generate the code and do not respond with any other messages. \
+                                "
+        },
+        {"role": "user",
+                 "content": feedback.question},
+        {"role": "system",
+                 "content": feedback.answer},
+        {"role": "user",
+                 "content": feedback.feedback},
     ]
 
     response = client.chat.completions.create(
